21c9ab27cfd190c299ba34e2e859ca2044b47ab2
toshi1014 <toshihide1014go@gmail.com>
2021-08-15 13:38:02 +0900

    initial commit

---
+++ generateMessage.ts
@@ -0,0 +1,10 @@
+class GenerateMessage{
+    constructor(){
+    }
+
+    getGeneratedMessage(sender: string): string{
+        return "Respect My Authoritah!!";
+    }
+}
+
+export default GenerateMessage;
