be94b3938ea5d5f5a0a76e9f56e54fb6082f85c6
toshi1014 <toshihide1014go@gmail.com>
2021-08-18 22:04:16 +0900

    add: MetaCharacters

---
+++ MetaCharacters.ts
@@ -0,0 +1,5 @@
+export const HEADER_PREFIX = "[LINE] ";
+export const HEADER_SUFFIX = "とのトーク履歴\n";
+export const STAMP_EMOTICON = "[スタンプ]";
+export const IMAGE = "[写真]";
+export const VIDEO = "[ビデオ]";
